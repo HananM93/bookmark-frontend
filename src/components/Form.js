943213462fd54cbd17739c5a32393e596d9f53b4
Ashton Bowens <ashxgrey11@gmail.com>
2023-01-04 22:15:08 -0800

    created bookmark add form

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,6 +3,11 @@ function Form() {
     return (
         <div>
             <h1>Add a new bookmark</h1>
+            <form action="">
+                <input type="text" id="webname" name="website" placeholder="Website"/>
+                <input type="text" id="link" name="link" placeholder="http://"/>
+                <input type="submit" value="Add"/>
+            </form>
         </div>
     )
 }
